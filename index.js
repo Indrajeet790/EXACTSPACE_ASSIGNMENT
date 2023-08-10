c00d69cbe7d6406d2cd3fca6c9e25165cd80504f
Vivek <vivekraj78965.vr@gmail.com>
2023-08-10 23:51:39 +0530

    basic setup of server and routes for getting form data and route for submiting json data and display json data

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressLayouts=require("express-ejs-layouts")
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
-const port = 3000;
+const PORT = 8000;
+const log = require("./filesync/largestfile");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//setup the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.static('./assets'));
+
+//using ejs 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
-
-app.get('/', (req, res) => {
-  res.render('form', { parsedData: null });
+// route to access form
+app.get("/", (req, res) => {
+  return res.render("form", { parsedData: null });
+});
+var jsonInput = null;
+// route for submit json data
+app.post("/submit-json", (req, res) => {
+  jsonInput = req.body.jsonInput;
+  const parsedData = JSON.parse(jsonInput);
+  return res.render("display_json", { parsedData });
 });
 
-let jsonInput = null; 
- 
-app.post('/submit-json', (req, res) => {
-    jsonInput = req.body.jsonInput;
-    const parsedData = JSON.parse(jsonInput); 
-    res.render("display_json" , { parsedData });
-  });
-
-app.get('/json', (req, res) => {
-    const parsedData = JSON.parse(jsonInput); 
-    res.render("view_data" , { parsedData });
-  });
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// route to display json as form
+app.get("/jsonAsForm", (req, res) => {
+  const parsedData = JSON.parse(jsonInput);
+  return res.render("view_data", { parsedData });
 });
+
+app.listen(PORT, (err) => {
+    if (err) {
+      console.log("error to run server");
+    } else {
+      console.log(`server is running on port ${PORT}`);
+    }
+  });
